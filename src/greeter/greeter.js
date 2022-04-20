6f495633d3472e7eace681386b77c55968294491
Marc H <marc@okaay.net>
2022-04-20 18:53:58 +0200

    specify locale

--- src/greeter/greeter.js
+++ src/greeter/greeter.js
@@ -1,6 +1,6 @@
 function greet(name) {
-  // function to return a greeting with the passed name and the current date without time
-  return `Hello ${name}, today is ${new Date().toLocaleDateString()}`;
+  // function to return a greeting with the passed name and the current date without time in german format
+  return `Hello ${name}, today is ${new Date().toLocaleDateString('de-DE')}`;
 }
 
 function mockMeFn(num) {
